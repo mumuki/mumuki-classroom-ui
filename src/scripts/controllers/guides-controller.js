67f4435091baf7b6730ca3a4e9f8851cc853d3a2
Federico Scarpa <fedescarpa@gmail.com>
2020-06-16 14:52:43 -0300

    Adapt new GuidesController to new guides response

--- src/scripts/controllers/guides-controller.js
+++ src/scripts/controllers/guides-controller.js
@@ -1,29 +1,27 @@
 
 angular
   .module('classroom')
-  .controller('GuidesController', function ($scope, $state, $stateParams, guides, Preferences, Breadcrumb, Notification, Domain) {
+  .controller('GuidesController', function ($scope, $state, $stateParams, guides, Guide, Chapter, Preferences, Breadcrumb, Notification, Domain) {
     Preferences($scope, 'lastChapter');
     if (_.isNil($scope.lastChapter)) $scope.lastChapter = { name: ''};
     $scope.setLastChapterOpened = (chapter) => { $scope.lastChapter = { name: chapter }};
 
     Breadcrumb.setCourse($stateParams.course);
-    const mapChapter = _.flow(_.lowerCase, _.deburr);
-    $scope.setCount(guides.length);
+
     $scope.actualChapter = (name) => name === $scope.lastChapter.name;
 
-    $scope.dict = _.groupBy(guides, 'parent.type');
-    $scope.chaptersAndLessons = _.groupBy($scope.dict.Lesson, (lesson) => mapChapter(lesson.parent.chapter.name));
-    $scope.exams = $scope.dict.Exam;
-    $scope.complements = $scope.dict.Complement;
-    $scope.others = $scope.dict.undefined;
+    $scope.exams = guides.exams.map((it) => Guide.from(it.guide));
+    $scope.chapters = guides.chapters.map((it) => Chapter.from(it));
+    $scope.complements = guides.complements.map((it) => Guide.from(it.guide));
+
     $scope.noItemsToShow = 'no_guides_to_show';
     $scope.inputPlaceholder = 'filter_available_guides';
 
-    $scope.sortCriteria = (guide) => ['parent.position', 'language.name', 'name'];
+    $scope.sortCriteria = () => ['number', 'name'];
     $scope.present = (items) => !!items;
 
-    $scope.open = (guide) => {
-      const [ org, repo ] = guide.slug.split('/');
+    $scope.open = (lesson) => {
+      const [ org, repo ] = lesson.slug.split('/');
       $state.go('classroom.courses.course.guides.guide', _.defaults({ org, repo }, $stateParams));
     };
 
@@ -32,8 +30,8 @@ angular
                            .groupBy('assignment.guide.slug')
                            .value();
 
-    $scope.notifications = (guide) => {
-      return _.get(notifications, guide.slug, []);
+    $scope.notifications = (lesson) => {
+      return _.get(notifications, lesson.slug, []);
     }
 
   });
