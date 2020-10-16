3d551c71db900d90fcff59f659d20030b50e3491
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-10-16 15:56:54 -0300

    Add mulyfill

---
+++ src/scripts/mulyfill.js
@@ -0,0 +1,20 @@
+window.mumuki = {};
+
+mumuki.exercise = {
+};
+
+mumuki.CustomEditor = {
+  addSource(source) {
+    // nothing
+  }
+};
+
+mumuki.kids = {
+  registerStateScaler(scaler) {
+    // nothing
+  },
+
+  registerBlocksAreaScaler(scaler) {
+    // nothing
+  }
+};
